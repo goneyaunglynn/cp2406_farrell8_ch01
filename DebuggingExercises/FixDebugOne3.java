7e37d435b65c97189597055112b4ca6009c5ce31
Gone Yaung Lynn <goneyaunglynn@gmail.com>
2017-07-20 14:10:48 +0800

    Prac01 solution

---
+++ DebuggingExercises/FixDebugOne3.java
@@ -0,0 +1,7 @@
+public class FixDebugOne3 {
+   public static void main(String[] args) {
+      System.Out.println("Over the river");
+      system.out.println("and through the woods");
+      System.out.println("to Grandmother's house we go");
+   }
+}
---
+++ DebuggingExercises/FixDebugOne4.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+public class FixDebugOne4 {
+    public static main(String[] args) {
+        JOptionPane.showMessageDialog(null, "First GUI program!")
+    }
+}
